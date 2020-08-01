e0ca164f24b3492ab8760026c72f8bd88a568dfa
cqh963852 <cqh963852@hotmail.com>
2020-08-02 01:17:16 +0800

    Update CRLF to LF

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,13 @@
+import "./index.css";
+
+import { configureStore } from "@reduxjs/toolkit";
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import * as serviceWorker from "./serviceWorker";
 import { Provider } from "react-redux";
-import { configureStore } from "@reduxjs/toolkit";
+
+import App from "./App";
 import rootReducer from "./reducer";
+import * as serviceWorker from "./serviceWorker";
 
 const store = configureStore({
   reducer: rootReducer,
