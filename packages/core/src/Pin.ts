3f62953caa18d5527f94a581405d31fc28600524
cqh963852 <cqh963852@hotmail.com>
2021-11-15 22:18:23 +0800

    update: update targetPin to connected

--- packages/core/src/Pin.ts
+++ packages/core/src/Pin.ts
@@ -7,13 +7,13 @@ class Pin implements IPin {
   id: string = uniqid();
   type: PIN_TYPE;
   parent: IChessman;
-  targetPin?: IPin;
+  connected?: IPin[] | IPin;
 
   constructor(pin: RequiredBy<Partial<IPin>, "type" | "parent">) {
     this.type = pin.type;
     this.parent = pin.parent;
     if (pin.id) this.id = pin.id;
-    if (pin.targetPin) this.targetPin = pin.targetPin;
+    if (pin.connected) this.connected = pin.connected;
   }
 }
 
@@ -23,7 +23,7 @@ export interface IPin {
   id: string;
   type: PIN_TYPE;
   parent: IChessman;
-  targetPin?: IPin;
+  connected?: IPin[] | IPin;
 }
 
 export enum PIN_TYPE {
